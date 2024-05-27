1ba6d4152d220f741759b9884f285000b406e79a
Isabel Motta <113033438+isabel-cmotta@users.noreply.github.com>
2024-05-27 10:15:54 -0300

    Delete firebase-init.ts

--- firebase-init.ts
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from "firebase/app";
-import firebaseConfig from './firebase-config';
-
-// Inicialização do Firebase
-const app = initializeApp(firebaseConfig);
-
-export default app;
