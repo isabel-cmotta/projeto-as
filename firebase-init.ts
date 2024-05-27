e5e10b95de01589938c1164b56bd56c58038dd1b
Isabel Motta <113033438+isabel-cmotta@users.noreply.github.com>
2024-05-27 10:07:21 -0300

    Add files via upload

---
+++ firebase-init.ts
@@ -0,0 +1,7 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from './firebase-config';
+
+// Inicialização do Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
